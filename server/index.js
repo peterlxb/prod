979a3073be822e450c2244d83b0415305d1e2840
peterlxb <peterlxb@gmail.com>
2018-03-23 18:30:54 +0800

    Need get access to accessToken

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
+const keys = require('./config/keys');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-passport.use(new GoogleStrategy())
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: "http://localhost:5000/auth/google/callback"
+},(accessToken) => {
+  console.log(accessToken)
+}));
 
+app.get('/auth/google/',
+  passport.authenticate('google', { scope: ['profile','email'] }),
+);
 
-app.listen(PORT,() => console.log('Example app listening on port 3000!'))
+app.get('/auth/google/callback',
+  passport.authenticate('google'),
+);
+
+app.listen(PORT,() => console.log('Example app listening on port 5000!'))
