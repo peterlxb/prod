5672b6c38c6a0b0ad85e3ddad672f7ec947ba3bb
peterlxb <peterlxb@gmail.com>
2018-03-24 13:43:34 +0800

    google oauth

--- server/index.js
+++ server/index.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-const keys = require('./config/keys');
+const keys = require('./config/keys.js');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//console.log(keys.googleClientID);
+//console.log(keys.googleClientSecret);
 
 passport.use(
   new GoogleStrategy({
     clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: "http://localhost:5000/auth/google/callback"
-},(accessToken) => {
-  console.log(accessToken)
+    clientSecret: keys.googleSecret,
+    callbackURL: "/auth/google/callback"
+},(accessToken,refreshToken,profile,done) => {
+  console.log(accessToken);
+  console.log(refreshToken);
 }));
 
 app.get('/auth/google/',
   passport.authenticate('google', { scope: ['profile','email'] }),
 );
 
-app.get('/auth/google/callback',
-  passport.authenticate('google'),
-);
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.listen(PORT,() => console.log('Example app listening on port 5000!'))
